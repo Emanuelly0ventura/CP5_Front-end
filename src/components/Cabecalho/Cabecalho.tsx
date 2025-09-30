6656374001947cdd0661d1fe1cf6f61dd81beae7
manu <haru.noom0@gmail.com>
2025-09-30 18:28:30 -0300

    criando componentes do projeto

---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,12 @@
+import Menu from "../Menu/Menu";
+
+
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <h1>Processamento de Produtos</h1>
+            <Menu/>
+        </header>
+    );
+}
