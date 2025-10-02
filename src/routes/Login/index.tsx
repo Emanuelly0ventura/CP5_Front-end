d90e7eb4ea7df7a627ecbe3158d211b88f27ec6a
carolnascgoncalves <carolrsc1411@gmail.com>
2025-10-01 21:34:20 -0300

    adicionando os formData no login

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,13 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  nomeUsuario: string;
+  email: string;
+};
 
 export default function Login(){
-
     document.title ="Login"
+    const methods = useForm();
+    const register = methods.register;
+    const handleSubmit = methods.handleSubmit;
+    const errors = methods.formState.errors;
 
-    return(
-        <main>
-            <h1>Login de usuario</h1>
-        </main>
-    );
+    const onSubmit = (data: FormData) => {
+        console.log("Dados invalidos: ", data);
+        alert(`Bem-vindo, ${data.nomeUsuario}!`);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h2>Login</h2>
+                <div>
+                    <label>Nome de usuario</label>
+                </div>
+
+            </form>
+
+        </div>
+    )
+
+
 
 }
 
