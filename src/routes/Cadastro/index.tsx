4717776f180bb50b42b45ecad7bafdae4bef3682
manu <haru.noom0@gmail.com>
2025-10-02 14:38:02 -0300

    fazendo JSON do cadastro

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,4 +1,5 @@
-
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 type CadastroData = {
   nome: string;
@@ -7,7 +8,29 @@ type CadastroData = {
 };
 
 export default function Cadastro(){
+    const { register, handleSubmit, formState: { errors } } = useForm<CadastroData>();
+    const navigate = useNavigate();
 
+    const onSubmit = async (data: CadastroData) => {
+    // verificar duplicidade
+    const res = await fetch(`http://localhost:3000/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`);
+    const users = await res.json();
+
+    if (users.length > 0) {
+      alert("Nome de usuário ou email já cadastrado!");
+      return;
+    }
+
+    // cadastrar no json-server
+    await fetch("http://localhost:3000/usuarios", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    alert("Cadastro realizado com sucesso!");
+    navigate("/login");
+  };
     
 
     return(
