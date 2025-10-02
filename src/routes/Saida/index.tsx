1c166399df9c8299de98cd5525a4b9ee19de6bd3
juliakina <julia.sayuri.kina@hotmail.com>
2025-10-02 20:08:40 -0300

    finalizei a pagina do redirecionamento do login, pagina principal e menu do petshop

--- src/routes/Saida/index.tsx
+++ src/routes/Saida/index.tsx
@@ -1,18 +1,32 @@
 import { useContext } from "react";
 import { UserContext } from "../../Providencia";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Saida() {
   const { user } = useContext(UserContext);
+  const navigate = useNavigate();
   return (
     <div>
         {(() => {
             if (user) {
-                return <p>Bem-vindo, {user.nomeUsuario} ({user.email})</p>;
+                return <h1>Bem-vindo!, {user.nome}🐹</h1>;
             } else {
                 return <p>Nenhum usuário logado</p>;
-            }   
+            }
         })()}
+
+        <h2>🐾 Bem-vindo ao Dias de Cão PetShop!🐾</h2>
+        <h3></h3>
+        <p>Escolha uma opção abaixo:</p>
+
+        <div>
+            <button onClick={() => navigate("")}>Marcar Consulta🐶</button>
+            <button onClick={() => navigate("")}>Ver Receita🐰</button>
+            <button onClick={() => navigate("")}>Itens da Loja🐱</button>
+            <button onClick={() => navigate("")}>Marcar Exames🐠</button>
+            <button onClick={() => navigate("")}>Avaliação🦜</button>
+        </div>
+
     </div>
   );
 }
