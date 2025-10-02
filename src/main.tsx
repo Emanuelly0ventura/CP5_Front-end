49a8edc87f4411c0a4283e2cb896e3d49b426975
juliakina <julia.sayuri.kina@hotmail.com>
2025-10-02 19:06:25 -0300

    concerto de login, fiz tratamento de erro, redirecionamento para a pagina saida

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Error from './routes/Error/index.tsx';
 import Cadastro from './routes/Cadastro/index.tsx';
 import Login from './routes/Login/index.tsx';
+import Saida from './routes/Saida/index.tsx';
+import { Providencias } from './Providencia.tsx';
 
 
 const router = createBrowserRouter([
@@ -12,13 +14,15 @@ const router = createBrowserRouter([
     path:"/", element: <App/>, errorElement: <Error/>, children:[
       {path:"/cadastro", element: <Cadastro/>},
       {path:"/login", element: <Login/>},
-      
+      { path: "/saida", element: <Saida /> }
     ]
   } 
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Providencias>
+      <RouterProvider router={router} />
+    </Providencias>
   </StrictMode>,
 )
--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,75 +1,66 @@
 import { useForm } from "react-hook-form";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type LoginData = {
   nomeUsuario: string;
   email: string;
 };
 
-export default function Login(){
-    const { register, handleSubmit, formState: { errors } } = useForm<LoginData>();
-    const navigate = useNavigate();
-    const onSubmit = async (data: LoginData) => {
+export default function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginData>();
+  const navigate = useNavigate();
 
+  const onSubmit = async (data: LoginData) => {
+    const res = await fetch(
+      `http://localhost:3000/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`
+    );
+    const users = await res.json();
 
-        const res = await fetch(`http://localhost:3000/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`);
-        const users = await res.json();
+    if (users.length > 0) {
+      localStorage.setItem("usuarioLogado", JSON.stringify(users[0]));
+      navigate("/Saida"); 
+    } else {
+      alert("Usuário ou email inválido!");
+    }
+  };
 
-        if (users.length > 0) {
-            localStorage.setItem("usuarioLogado", JSON.stringify(users[0]));
-            navigate("/cadastro");
-        } else {
-            alert("Usuário ou email inválido!");
-        }
-    };
-
-    return (
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Login</h2>
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h2>Login</h2>
-                <div>
-                    <label>Nome de usuario</label>
-                    <input 
-                        type="text" 
-                        {...register("nomeUsuario", {
-                            required: "O nome de usuário é obrigatorio",
-                            minLength: {
-                                value: 3,
-                                message: "O nome deve ter pelo menos 3 caracteres",
-                            },
-                        })}
-                    />
-                    {errors.nomeUsuario && (
-                        <p>
-                            {errors.nomeUsuario.message} 
-                        </p>
-                    )}
-
-                </div>
-
-                <div>
-                    <label>Email</label>
-                    <input type="email"
-                        {...register("email", {
-                            required: "O email é obrigatório",
-                            pattern: {
-                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                                message: "Formato de email inválido",
-                            },
-                        })}
-                    />
-                    {errors.email && (
-                        <p>{errors.email.message}</p>
-                    )}
-                </div>
-
-                <button type="submit">Entrar</button>
-                
-                
-            </form>
-
+          <label>Nome de usuário</label>
+          <input
+            type="text"
+            {...register("nomeUsuario", {
+              required: "O nome de usuário é obrigatório",
+              minLength: {
+                value: 3,
+                message: "O nome deve ter pelo menos 3 caracteres",
+              },
+            })}
+          />
+          {errors.nomeUsuario && <p>{errors.nomeUsuario.message}</p>}
         </div>
-    )
 
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "O email é obrigatório",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Formato de email inválido",
+              },
+            })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
 }
 
