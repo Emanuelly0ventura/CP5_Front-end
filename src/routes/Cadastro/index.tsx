1aaa5bdf788c25940e912b2de733e795119faa85
carolnascgoncalves <carolrsc1411@gmail.com>
2025-10-04 17:52:39 -0300

    Estilizando a página de cadastro com tailwindcss

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -38,35 +38,37 @@ export default function Cadastro() {
   };
 
   return (
-    <main className="cadastro-main">
-      <div className="universal-form">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="universal-title">Cadastro</h2>
-          
-          <div className="universal-field">
-            <label className="universal-label">Nome</label>
-            <input className="universal-input" {...register("nome", { required: "Nome obrigatório" })} />
-            {errors.nome && <p className="universal-error">{errors.nome.message}</p>}
-          </div>
+    <main>
 
-          <div className="universal-field">
-            <label className="universal-label">Nome de usuário</label>
-            <input className="universal-input" {...register("nomeUsuario", { required: "Nome de usuário obrigatório" })} />
-            {errors.nomeUsuario && <p className="universal-error">{errors.nomeUsuario.message}</p>}
-          </div>
+      <h1 className="universal-title">Cadastro</h1>
+      <div className="universal-card"> 
+        <div className="universal-form">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="universal-field">
+              <label className="universal-label">Nome</label>
+              <input className="universal-input" placeholder="Digite o seu nome..." {...register("nome", { required: "Nome obrigatório" })} />
+              {errors.nome && <p className="universal-error">{errors.nome.message}</p>}
+            </div>
 
-          <div className="universal-field">
-            <label className="universal-label">Email</label>
-            <input className="universal-input" type="email" {...register("email", {required: "Email obrigatório",pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Email inválido" }})}/>
-            {errors.email && <p className="universal-error">{errors.email.message}</p>}
-          </div>
+            <div className="universal-field">
+              <label className="universal-label">Nome de usuário</label>
+              <input className="universal-input" placeholder="Digite o nome de usuário..."{...register("nomeUsuario", { required: "Nome de usuário obrigatório" })} />
+              {errors.nomeUsuario && <p className="universal-error">{errors.nomeUsuario.message}</p>}
+            </div>
 
-          <button className="universal-btn" type="submit">Cadastrar</button>
+            <div className="universal-field">
+              <label className="universal-label">Email</label>
+              <input className="universal-input" placeholder="Digite o seu email..." type="email" {...register("email", {required: "Email obrigatório",pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Email inválido" }})}/>
+              {errors.email && <p className="universal-error">{errors.email.message}</p>}
+            </div>
 
-        </form>
+            <div className="universal-div-btn">
+              <button className="universal-btn" type="submit">Cadastrar</button>
+            </div>
+          </form>
 
+        </div>
       </div>
-
     </main>
     
   );
