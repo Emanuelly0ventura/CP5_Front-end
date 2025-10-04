6d297d87707fa1cbec9c71da4498b75486678990
juliakina <julia.sayuri.kina@hotmail.com>
2025-10-04 18:17:20 -0300

    Estilizando página de erro com tailwindcss

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,22 +1,22 @@
 import { useEffect } from "react";
 import { RiArrowGoBackFill as VoltarHome } from "react-icons/ri";
-import { BiError as ErrorIcon} from "react-icons/bi";
+import { LuConstruction as Construcao } from "react-icons/lu";
 import { Link } from "react-router-dom";
 
 export default function Error() {
     useEffect(() => {
-        document.title = "404 - Página não encontrada";
+        document.title = "Página em construção!";
     }, []);
 
     return (
         <main className="error-page">
             <div className="error-container">
-                <ErrorIcon className="error-icon" />
-                <h1 className="error-title">404</h1>
+                <Construcao className="error-icon" />
+                <h1 className="error-title">Página em construção</h1>
                  <p className="error-message">
-                    Ops! A página que você tentou acessar não foi encontrada.
+                    Ops! A página que você tentou acessar está em construção.
                 </p>
-                <Link to="/cadastro" className="error-button">Voltar para a página inicial<VoltarHome className="error-button-icon"/>
+                <Link to="/login" className="error-button">Voltar para a página de login<VoltarHome className="error-button-icon"/>
                 </Link>
             </div>
         </main>
